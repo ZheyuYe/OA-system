958b17afef8c326ce5a43bb8cd276853933deea6
georgedreamfar <georgedreamfar@gmail.com>
2017-06-09 08:37:41 +0800

    ..

---
+++ src/main/java/com/giit/www/entity/PayMent.java
@@ -0,0 +1,67 @@
+package com.giit.www.entity;
+
+public class PayMent {
+    int id;
+    String apply_name;
+    String handle_name;
+    String apply_reason;
+    String apply_money;
+    String handle_time;
+    String handle_state;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApply_name() {
+        return apply_name;
+    }
+
+    public void setApply_name(String apply_name) {
+        this.apply_name = apply_name;
+    }
+
+    public String getHandle_name() {
+        return handle_name;
+    }
+
+    public void setHandle_name(String handle_name) {
+        this.handle_name = handle_name;
+    }
+
+    public String getApply_reason() {
+        return apply_reason;
+    }
+
+    public void setApply_reason(String apply_reason) {
+        this.apply_reason = apply_reason;
+    }
+
+    public String getApply_money() {
+        return apply_money;
+    }
+
+    public void setApply_money(String apply_money) {
+        this.apply_money = apply_money;
+    }
+
+    public String getHandle_time() {
+        return handle_time;
+    }
+
+    public void setHandle_time(String handle_time) {
+        this.handle_time = handle_time;
+    }
+
+    public String getHandle_state() {
+        return handle_state;
+    }
+
+    public void setHandle_state(String handle_state) {
+        this.handle_state = handle_state;
+    }
+}
---
+++ src/main/java/com/giit/www/entity/QJ.java
@@ -0,0 +1,76 @@
+package com.giit.www.entity;
+
+public class QJ {
+    int id;
+    String apply_name;
+    String handle_name;
+    String apply_reason;
+    String begin_time;
+    String end_time;
+    String handle_time;
+    String handle_state;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getApply_name() {
+        return apply_name;
+    }
+
+    public void setApply_name(String apply_name) {
+        this.apply_name = apply_name;
+    }
+
+    public String getHandle_name() {
+        return handle_name;
+    }
+
+    public void setHandle_name(String handle_name) {
+        this.handle_name = handle_name;
+    }
+
+    public String getApply_reason() {
+        return apply_reason;
+    }
+
+    public void setApply_reason(String apply_reason) {
+        this.apply_reason = apply_reason;
+    }
+
+    public String getBegin_time() {
+        return begin_time;
+    }
+
+    public void setBegin_time(String begin_time) {
+        this.begin_time = begin_time;
+    }
+
+    public String getEnd_time() {
+        return end_time;
+    }
+
+    public void setEnd_time(String end_time) {
+        this.end_time = end_time;
+    }
+
+    public String getHandle_time() {
+        return handle_time;
+    }
+
+    public void setHandle_time(String handle_time) {
+        this.handle_time = handle_time;
+    }
+
+    public String getHandle_state() {
+        return handle_state;
+    }
+
+    public void setHandle_state(String handle_state) {
+        this.handle_state = handle_state;
+    }
+}
